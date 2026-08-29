b699503dd0af5018d657f992b46c00ba1e444589
Yuu Komiya <yuu.komiya@n138.jp>
2026-08-29 16:26:50 +0900

    genshin_status_log_view

--- container-db-build/table-genshin_status_log.sql
+++ container-db-build/table-genshin_status_log.sql
@@ -24,9 +24,27 @@ CREATE OR REPLACE TRIGGER update_genshin_status_log_updated_at
 -- VIEW
 CREATE OR REPLACE VIEW genshin_status_log_view AS
     SELECT
-        g.id index,
-        g.created_at,
-        g.updated_at,
-        g.rawjson->'enka'->>'uid' uid
+        id AS index,
+        TO_CHAR(
+            UPDATED_AT AT TIME ZONE 'Asia/Tokyo',
+            'FMMM/DD FMHH24:MI:SS'
+        ) AS updated_at,
+        rawjson->'enka'->>'uid' AS uid,
+        rawjson->'enka'->'playerInfo'->>'nickname' AS nickname,
+        rawjson->'enka'->'playerInfo'->>'signature' AS signature,
+        rawjson->'hoyolab'->>'current_resin' AS CURRENT_RESIN,
+        rawjson->'hoyolab'->>'max_resin' AS MAX_RESIN,
+        RTRIM(
+            TO_CHAR(
+                ((rawjson->'hoyolab'->>'current_resin')::NUMERIC / (rawjson->'hoyolab'->>'max_resin')::NUMERIC * 100),
+                'FM990.99'
+            ),
+            '.'
+        ) || '%' AS CURRENT_RESIN_PERCENT,
+        -- 24時間以上の回復時間にも対応する場合（日・時間・分）
+        TO_CHAR(
+            ((rawjson->'hoyolab'->>'resin_recovery_time')::BIGINT * '1 second'::INTERVAL),
+            'FMDD"d "HH24:MI:SS'
+        ) AS RESIN_RECOVERY_TIME
     FROM
-        genshin_status_log g;
+        genshin_status_log;
