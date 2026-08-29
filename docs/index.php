cd8bb4184189b83079b30ff194625b2e3f711c5e
Yuu Komiya <yuu.komiya@n138.jp>
2026-08-29 12:54:55 +0900

    $payload_json to compact

--- docs/index.php
+++ docs/index.php
@@ -260,7 +260,7 @@ function main(){
 
   /* * Notice to Discord * */
   {
-    $payload_json = json_encode($discord_notifies_payload_data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    $payload_json = json_encode($discord_notifies_payload_data);
     if(mb_strlen($payload_json)<=2000) {
       $ch = curl_init($v['discord']['webhook']['url'].'?wait=true');
       curl_setopt($ch, CURLOPT_POST, true);
